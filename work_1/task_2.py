526c23a27ae370b1e78ba2a1998661ad3c871391
Just Sir <justflud@mail.ru>
2022-02-02 20:37:42 +0300

    Creating base template

---
+++ work_1/task_2.py
@@ -0,0 +1,22 @@
+import pygame
+from pygame.draw import *
+
+pygame.init()
+
+FPS = 30
+screen = pygame.display.set_mode((2048, 1024))
+#screen.fill((220,220,220))
+
+
+
+pygame.display.update()
+clock = pygame.time.Clock()
+finished = False
+
+while not finished:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+
+pygame.quit()
