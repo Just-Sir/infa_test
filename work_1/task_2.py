b2110d7bb595389fa1afe6805b9de67eef0cb574
Just Sir <justflud@mail.ru>
2022-02-03 16:51:09 +0300

    Added background

--- work_1/task_2.py
+++ work_1/task_2.py
@@ -1,13 +1,34 @@
+import random
 import pygame
 from pygame.draw import *
 
+class Background:
+    def __init__(self, n_stripes):
+        self.n = n_stripes
+        self.width_stripe = HEIGTH / self.n
+                
+    def draw(self):
+        for i in range(self.n):
+            rect(
+            screen, (self.colorize()),
+            (0, self.width_stripe*i, WIDTH, self.width_stripe)
+            )
+             
+    def colorize(self):
+        color = ()
+        for i in range(3):
+            color += (random.randint(0, 255),)
+        return color    
+  
+
 pygame.init()
 
+WIDTH, HEIGTH = 1280, 500
 FPS = 30
-screen = pygame.display.set_mode((2048, 1024))
-#screen.fill((220,220,220))
+bg = Background(5)
+screen = pygame.display.set_mode((WIDTH, HEIGTH))
 
-
+bg.draw()
 
 pygame.display.update()
 clock = pygame.time.Clock()
